2008056d920f0b803855eda92cc5345ac8db8595
ip <ivan.poluianov@gmail.com>
2025-05-20 16:49:03 +0200

    btn

--- ui/button.go
+++ ui/button.go
@@ -1,8 +1,6 @@
 package ui
 
 import (
-	"image/color"
-
 	"github.com/ipoluianov/nui/nuimouse"
 )
 
@@ -10,31 +8,25 @@ func NewButton() *Widget {
 	c := NewWidget()
 	c.SetSize(100, 30)
 	c.SetOnPaint(func(cnv *Canvas) {
-		cnv.SetColor(color.RGBA{0, 0, 0, 255})
-		backColor := color.RGBA{0, 0, 0, 255}
+		backColor := GetThemeColor("background", DefaultBackground)
 		if c.IsHovered() {
-			backColor = color.RGBA{50, 50, 50, 255}
-			clicked := c.GetPropBool("clicked", false)
-			if clicked {
-				backColor = color.RGBA{100, 100, 100, 255}
+			backColor = GetThemeColor("button.background.hover", DefaultBackground)
+			if c.GetPropBool("pressed", false) {
+				backColor = GetThemeColor("button.background.pressed", DefaultBackground)
 			}
 		}
-
 		cnv.FillRect(0, 0, c.W(), c.H(), backColor)
-
-		text := c.GetPropString("text", "")
-
-		cnv.DrawTextMultiline(0, 0, c.W(), c.H(), HAlignCenter, VAlignCenter, text, color.RGBA{255, 255, 255, 255}, "robotomono", 16, false)
+		cnv.DrawTextMultiline(0, 0, c.W(), c.H(), HAlignCenter, VAlignCenter, c.GetPropString("text", ""), GetThemeColor("foreground", DefaultForeground), "robotomono", 16, false)
 	})
 
 	c.SetMouseCursor(nuimouse.MouseCursorPointer)
 
 	c.SetOnMouseDown(func(button nuimouse.MouseButton, x int, y int) {
-		c.SetProp("clicked", true)
+		c.SetProp("pressed", true)
 	})
 
 	c.SetOnMouseUp(func(button nuimouse.MouseButton, x int, y int) {
-		c.SetProp("clicked", false)
+		c.SetProp("pressed", false)
 		if MainForm.hoverWidget == c {
 			fnOnClick, ok := c.GetProp("onClick").(func())
 			if ok {
@@ -42,8 +34,6 @@ func NewButton() *Widget {
 			}
 		}
 	})
-
-	//fnOnClick := c.GetProp("onClick")
 
 	return c
 }
